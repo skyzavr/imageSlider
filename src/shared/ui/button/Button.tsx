4188dde3e1508bafecc1ea2cd355e398f196cd8d
nino <dankozero111@gmail.com>
2024-10-03 22:42:13 +0300

    [style]: fix css styles

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -1,24 +1,43 @@
 import classNames from 'classnames';
 
+import { widthTapeFrame as width } from '@features/Tape/lib/constant';
 import { icons } from '../Icon/Icon';
+
 import classes from './button.module.css';
 
 type btnProps = {
-  isVisible: boolean;
+  isVisible?: boolean;
   btnIcon: string;
   onClickHandler: () => void;
+  widthTapeFrame?: number;
+  isTop?: boolean;
   isDisabled?: boolean;
+  isInTape?: boolean;
 };
 
 export const Button = (btnProps: btnProps) => {
-  const { isVisible, btnIcon, onClickHandler, isDisabled = false } = btnProps;
+  const {
+    isVisible = true,
+    btnIcon,
+    onClickHandler,
+    widthTapeFrame = width,
+    isTop = true,
+    isDisabled = false,
+    isInTape = false,
+  } = btnProps;
+
   const cl = classNames(
     classes.button,
     isVisible ? classes.isVisible : '',
-    isDisabled ? classes.disabled : ''
+    isDisabled ? classes.disabled : '',
+    isInTape ? classes.inTape : '',
+    isTop ? classes.top : classes.bottom
   );
+
+  const widthStyle = { width: `${widthTapeFrame}px` };
+
   return (
-    <button onClick={onClickHandler} className={cl}>
+    <button style={widthStyle} onClick={onClickHandler} className={cl}>
       {icons[btnIcon]}
     </button>
   );
