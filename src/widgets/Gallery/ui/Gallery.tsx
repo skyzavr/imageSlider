4bc88741c01ad92af7403cce579f9715c8cc748c
nino <dankozero111@gmail.com>
2024-10-03 19:27:56 +0300

    [add]: add shot context && slider widget depends on screen's width

--- src/widgets/Gallery/ui/Gallery.tsx
+++ src/widgets/Gallery/ui/Gallery.tsx
@@ -1,14 +1,18 @@
+import { ShotContextProvider } from '@app/api/context/shot/ShotContext';
+
 import { DesktopGallery } from '@widgets/DesktopGallery';
 import { MobileGallery } from '@widgets/MobileGallery';
-import { desktopMinWidth } from '@shared/config/constant';
+import { desktopMinWidth } from '@shared/lib/constant';
 
 import classes from './gallery.module.css';
 
 export const GalleryWrapper = () => {
   const isDesktopScreen = window.innerWidth >= desktopMinWidth;
+  const slider = isDesktopScreen ? <DesktopGallery /> : <MobileGallery />;
+
   return (
     <div className={classes.wrapper}>
-      {isDesktopScreen ? <DesktopGallery /> : <MobileGallery />}
+      <ShotContextProvider>{slider}</ShotContextProvider>
     </div>
   );
 };
