6eaa98a39ee3124d8d8de295c110e5438cc60758
nino <dankozero111@gmail.com>
2024-10-03 14:55:18 +0300

    [add]: main app wrapper

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,16 @@
+import { Header } from '@shared/ui';
+import { GalleryWrapper } from '@widgets/Gallery';
+
 const App = () => {
-  return <>Hello</>;
+  return (
+    <>
+      <Header />
+      <main>
+        <section>
+          <GalleryWrapper />
+        </section>
+      </main>
+    </>
+  );
 };
 export default App;
