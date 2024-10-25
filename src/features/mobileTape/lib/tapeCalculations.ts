2d80fd5a43bb6027c44089f5fed473ab6e649337
nino <dankozero111@gmail.com>
2024-10-25 16:30:11 +0300

    [feat]: func, that calculates number of pic-s in the tape

--- src/features/mobileTape/lib/tapeCalculations.ts
+++ src/features/mobileTape/lib/tapeCalculations.ts
@@ -1,2 +1,8 @@
 const padding = 20;
+
+export const imgTapeMobile = 64;
+
+export const calcPicInTheTape = (tapeWidth: number) =>
+  Math.floor(tapeWidth / imgTapeMobile);
+
 export const totalWidth = (innerWidth: number) => innerWidth - padding * 2;
