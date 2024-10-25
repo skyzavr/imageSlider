c0df40324073b78b0121a3eacc2dbc3ba294455a
nino <dankozero111@gmail.com>
2024-10-25 16:31:46 +0300

    [feat]: add new constant

--- src/shared/lib/constant.ts
+++ src/shared/lib/constant.ts
@@ -2,5 +2,9 @@ export const desktopMinWidth = 1080;
 
 export type modalProps = {
   isShow: boolean;
+  tapeImgNum: number;
   toggleModal: () => void;
 };
+
+//amount of imgs that shows in the slider tape in desktop version
+export const tapeImgNumber = 6;
